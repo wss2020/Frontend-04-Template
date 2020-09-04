0121feac65987e3215d14dbe448faaca2913465f
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-09-04 13:24:39 +0800

    寻路

---
+++ Week_02/test.js
@@ -0,0 +1,41 @@
+function takeLongTime(n) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(n + 200), n);
+    });
+}
+function step1(n) {
+    console.log(`step1 with ${n}`);
+    return takeLongTime(n);
+}
+function step2(m, n) {
+    console.log(`step2 with ${m} and ${n}`);
+    return takeLongTime(m + n);
+}
+function step3(k, m, n) {
+    console.log(`step3 with ${k}, ${m} and ${n}`);
+    return takeLongTime(k + m + n);
+}
+
+
+async function doIt() {
+    console.time("doIt");
+    const time1 = 300;
+    const time2 = await step1(time1);
+    const time3 = await step2(time1, time2);
+    const result = await step3(time1, time2, time3);
+    console.log(`result is ${result}`);
+    console.timeEnd("doIt");
+}
+doIt();
+// c:\var\test>node --harmony_async_await .
+// step1 with 300
+// step2 with 800 = 300 + 500
+// step3 with 1800 = 300 + 500 + 1000
+// result is 2000
+// doIt: 2907.387ms
+
+
+
+
+
+

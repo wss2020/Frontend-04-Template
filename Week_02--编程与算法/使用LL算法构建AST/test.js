4cffee49ce1c01d01eff1cff8cf4acfc3ec45ab2
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-09-06 12:28:09 +0800

    LL算法

---
+++ Week_02--编程与算法/使用LL算法构建AST/test.js
@@ -0,0 +1,10 @@
+const regex1 = RegExp('foo*', 'g');
+const str1 = 'table football, foosball';
+
+console.log( regex1.exec(str1)  );
+console.log( regex1.exec(str1)  );
+console.log( regex1.exec(str1)  );
+
+
+
+

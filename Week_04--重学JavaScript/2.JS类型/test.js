dbabf497173139e7308e29a175b2be85752f413a
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-09-20 19:32:21 +0800

    重学 JavaScript

---
+++ Week_04--重学JavaScript/2.JS类型/test.js
@@ -0,0 +1,11 @@
+
+
+
+console.log(   1/3  );
+console.log(   1/33  );
+
+// console.log(  88.toString()   );   // 错误
+console.log(  88 .toString()   );   // 对的
+
+let result = 88 .toString();
+console.log( typeof result);

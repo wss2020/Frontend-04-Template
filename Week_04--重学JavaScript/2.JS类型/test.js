32fc6cb7e97a8a1cf8e3b98d2e829fb2bec4a2ee
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-09-21 20:33:07 +0800

    重学 JavaScript

--- Week_04--重学JavaScript/2.JS类型/test.js
+++ Week_04--重学JavaScript/2.JS类型/test.js
@@ -9,3 +9,19 @@ console.log(  88 .toString()   );   // 对的
 
 let result = 88 .toString();
 console.log( typeof result);
+
+
+function f(){
+    var undefined = 1;
+    console.log(undefined);
+}
+
+f();
+
+
+// 下面写法是错误的
+// function f1(){
+//     var null = 1;
+//     console.log(null);
+// }
+

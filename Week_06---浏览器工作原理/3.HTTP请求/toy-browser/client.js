05cda5153dd122b641de4c916ce2579f1fb04116
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-10-05 11:59:13 +0800

    HTTP 请求

--- Week_06---浏览器工作原理/3.HTTP请求/toy-browser/client.js
+++ Week_06---浏览器工作原理/3.HTTP请求/toy-browser/client.js
@@ -1,5 +1,4 @@
 const net = require("net");
-
 class Request {
     constructor(options) {
         this.method = options.method || "GET";
@@ -11,6 +10,7 @@ class Request {
         if(!this.headers["Content-Type"]){
             this.headers["Content-Type"] = "application/x-www-form-urlencoded";
         }
+
         if(this.headers["Content-Type"] === "application/json")
             this.bodyText = JSON.stringify(this.body);
         else if(this.headers["Content-Type"] === "application/x-www-form-urlencoded")
@@ -18,14 +18,117 @@ class Request {
                 .map(key => `${key}=${encodeURIComponent(this.body[key])}`)
                 .join('&');
         this.headers["Content-Length"] = this.bodyText.length;
-
-        console.log(this.headers);
     }
 
-    send(){
+    send(connection){
         return new Promise((resolve,reject)=>{
-            // ....
+            const parser = new ResponseParser;
+            if(connection){
+                connection.write(this.toString());
+            }else{
+                connection = net.createConnection({
+                    port: this.port,
+                },()=>{
+                    connection.write(this.toString());
+                })
+            }
+            connection.on('data',(data)=>{
+                console.log(data.toString());
+                parser.receive(data.toString());
+                if(parser.isFinished){
+                    resolve(parser.response);
+                    connection.end();
+                }
+            })
+            connection.on('err',(err)=>{
+                reject(err);
+                connection.end();
+            })
         })
+    }
+
+    toString(){
+        return `${this.method} ${this.path} HTTP/1.1\r
+            ${Object.keys(this.headers).map(key=> `${key}: ${this.headers[key]}`).join('\r\n')}\r
+            \r
+            ${this.bodyText}
+            `
+    }
+}
+
+class ResponseParser {
+    constructor() {
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 5;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = "";
+        this.headers = {};
+        this.headerName = "";
+        this.headerValue = "";
+        this.bodyParser = "";
+    }
+    receive(string){
+        for(let i=0; i<string.length; i++){
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char){
+        if(this.current === this.WAITING_STATUS_LINE){
+            if(char === '\r'){
+                this.current  = this.WAITING_HEADER_LINE_END;
+            }else{
+                this.statusLine += char;
+            }
+        }else if(this.current === this.WAITING_STATUS_LINE_END){
+            if(char === '\n'){
+                this.current  = this.WAITING_HEADER_NAME;
+            }
+        }else if(this.current === this.WAITING_HEADER_NAME){
+            if(char === ':'){
+                this.current = this.WAITING_HEADER_SPACE;
+            }else if(char === '\r'){
+                this.current = this.WAITING_HEADER_BLOCK_END;
+            }else{
+                this.headerName += char;
+            }
+        }else if(this.current === this.WAITING_HEADER_VALUE){
+            if(char === '\r'){
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = "";
+                this.headerValue = "";
+            }else{
+                this.headerValue += char;
+            }
+        }else if(this.current === this.WAITING_HEADER_SPACE){
+            if(char === ' '){
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        }else if(this.current === this.WAITING_HEADER_VALUE){
+            if(char === '\r'){
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = "";
+                this.headerValue = "";
+            }else{
+                this.headerValue += char;
+            }
+        }else if(this.current === this.WAITING_HEADER_LINE_END){
+            if(char === '\n'){
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        }else if(this.current === this.WAITING_HEADER_BLOCK_END){
+            if(char === '\n'){
+
+            }
+        }
     }
 }
 
@@ -39,8 +142,7 @@ void async function () {
             ["X-Foo2"]: "customed"
         },
         body: {
-            name: "winter",
-            sex: "male"
+            name: "winter"
         }
     });
     let response = await request.send();

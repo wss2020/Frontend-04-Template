42865b748e90c261625e25465fbc97a5f176d2d4
王帅 <wangshuaishuai@yidiansongli.com>
2020-10-09 13:10:45 +0800

    HTML 解析

--- Week_07---浏览器工作原理/1 HTML解析/4 创建元素/parser.js
+++ Week_07---浏览器工作原理/1 HTML解析/4 创建元素/parser.js
@@ -1,11 +1,25 @@
+let currentToken = null;
+
+function emit(token) {
+    // if(token.type!="text")
+    console.log(token);
+}
+
 const EOF = Symbol("EOF");    // EOF: End Of File
 
 function data(c) {
     if (c == "<") {
         return tagOpen;
     } else if (c == EOF) {
+        emit({
+            type: "EOF"
+        });
         return;
     } else {
+        emit({
+            type: "text",
+            content: c
+        });
         return data;
     }
 }
@@ -14,7 +28,11 @@ function tagOpen(c) {
     if (c == "/") {
         return endTagOpen;
     } else if (c.match(/^[a-zA-Z]$/)) {
-        return tagName;
+        currentToken = {
+            type: "startTag",
+            tagName: ""
+        }
+        return tagName(c);
     } else {
         return;
     }
@@ -22,6 +40,10 @@ function tagOpen(c) {
 
 function endTagOpen(c) {
     if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: "endTag",
+            tagName: ""
+        }
         return tagName(c);
     } else if (c == ">") {
 
@@ -38,8 +60,10 @@ function tagName(c) {
     } else if (c == "/") {
         return selfClosingStartTag;
     } else if (c.match(/^[a-zA-Z]$/)) {
+        currentToken.tagName += c  //.toLowerCase()
         return tagName;
     } else if (c == ">") {
+        emit(currentToken);
         return data;
     } else {
         return tagName;
@@ -58,8 +82,15 @@ function beforeAttributeName(c) {
     }
 }
 
-function selfClosingStartTag(c){
+function selfClosingStartTag(c) {
+    if (c == ">") {
+        currentToken.isSelfClosing = true;
+        return data;
+    } else if (c == "EOF") {
 
+    } else {
+
+    }
 }
 
 
@@ -72,39 +103,47 @@ module.exports.parseHTML = function parseHTML(html) {
 }
 
 /****
- data 状态我们是一个初始状态，data 状态我们首先要关心的它是不是一个 tag ,那么 tag 它的特点是一个以小于号 也就是左尖括号 < 为
- 开始的标志的，所以我们在 data 里面首先要识别的一个字符就是一个小于号，也就是左尖括号 <， 去区分这三种标签，除了左尖括号，那么如果是
- EOF 的话，那么在 data 状态应该是我们会返回一个结束的状态，我们就直接截止了，那么如果再调这个就说明报错了，因为文件结束以后我们就不可
- 能再收到数据了，除了左尖括号之外的所有字符，我们都可以把它理解为是文本节点，那么咱们先处理 tag , 文本节点咱们一会再说，所以说在这里我
- 们就相当于忽略了所有的小于号的符号了，一旦遇到小于号，我们就会进入到 tagOpen 状态，在 tagOpen 状态，这个时候我们还不知道它是三种
- tag 的哪一种，注意，这是标签开始，不是开始标签，这个时候我们还不知道它是三种标签类型之中的哪一种，
- 然后我们来看看 tagOpen 状态，那么我们是首先有一个判断它是不是结束标签，结束标签的特点就是它在左尖括号之后，紧跟着一个正斜杠，所以
- 在 tagOpen 状态，我们首先要判断的就是正斜杠，一旦检测到了正斜杠，那么我们就会进入到一个叫做 endTagOpen 的状态，也就是结束标签的开头，
- 如果它是一个英文的字母，那么我们就会发现要么是一个开始标签（这里我觉得是一个结束标签），要么是一个自封闭标签，所以我们就会进入到一个 tagName
- 状态，这个时候我们收集它的 tagName ,
-    然后如果我们进入到 endTagOpen 状态，我们同样也要找这个 endTag 的 tagName ，endTag 的 tagName 那么它是一样也是包含小写字母 或者
- 是大写字母的，所以它同样会进入到一个 tagName 的状态，如果 endTagOpen 之后，紧跟着一个大于号，也就是右尖括号 > ,那么要报错，如果这个
- endTagOpen 之后出现了 EOF ，那么它也会报错，那么这个就是一个我们正常的这样的 处理异常的一个逻辑，
-    然后我们来看看 tagName 这个地方，tagName 它是以什么结束的，它肯定是以空白符结束的，在 HTML 里面有效的空白符有 4 种，分别是 tab 符、
- 换行符、禁止符 和 空格， 这个 \f 是 prohibited ， \n 是换行， 然后我们如果遇到空格了，那么大家都写过 HTML 标签，如果我们遇到了一个空格，
- 比如说  <html prop    第一个如果是大于号，那么它会进入到一个开始标签的状态，如果这个地方有一个空格，那么它就后面要跟属性了，对不对，所以我们
- 可以认为 tagName 它是以左尖括号开始，以空格结束的这样的一段状态，那么所以我们当在 tagName 里面，遇到空格的时候，我们就会进入到一个新的状态
- 叫做 beforeAttributeName ，如果我们遇到了一个正斜杠 / ，那么就说明这是一个标签名，然后后面有一个正斜杠，这说明它是一个自封闭的标签，那么我们
- 就会进入到一个 selfClosingStartTag 这样的一个状态，当然如果 tagName 里面又加了这个字符的话，那么它还是在 tagName 里面，然后如果是右尖括号，
- 那说明它就是一个普通的开始标签，所以我们就会结束掉这个标签，然后回到 data 状态，然后来解析下一个标签，这就是所谓的 tagName 里面的逻辑了。
-    然后 beforeAttributeName ，我们这里暂时不去对它进行处理，实际上 beforeAttribute 里面，我们会期待遇到一个属性名，或者是一个正斜杠之类的
- 这样的东西，但是我们暂时不处理属性，那么我们就先给它所有的可能性，都给他 return AttributeName ,除了大于号 > 可以作为终止标志，那么其他的我们
- 就都等于把这个字符给它吃掉，也就是说在 beforeAttributeName 里面，我们就死等这个 右尖括号 >， 然后让它去结束，然后 selfClosingStarTag，那么
- 我们分析到一个标签， <div /   已经分析到有一个杠了，后面它就只有大于号是有效的，其他的都会报错，这里报错的逻辑我们就都先不写了，我们只是不 return
-    然后这就是一个状态机的雏形，跑起来看看。
+ 来看代码，那么首先 ParseHTML 里面的代码是一点都没有动的，我们现在要做的只是在每一个 state 的状它机里加入
+ 我们的这样的业务逻辑，
+ 首先我们要实现一个 emit(token) 这样的一个函数，因为我们最后这个状态机里面所有的状态，创建完 token 之后，
+ 我们要把它在同一个出口给它输出，所以我们写一个全局的这样的一个 emit(token) 函数，而这里我们先把它做一次
+ console.log ,等到后面我们再研究怎么样去处理它，其他的代码没有什么变化，然后我们要有一个全局变量叫做，是因为我们
+ 在 HTML 里面，tag 不管有多复杂，那么它是当做一个 token 去处理的，那么所以我们要随着状态机一个一个的读进来字符
+ 的时候，逐步的去构造 token 里面的内容，这个跟我们前面对状态机的使用风格也是类似的，
+ 那么我们首先先看一下在 data 状态，如果等于 EOF 的话，我们会提交一个 EOF token ,然后如果是文本节点，那么
+ 我们会 emit 一个 text token ,那么 content 就是文本节点里面的一个字符，这一步我们会去一个一个字符的把它 emit
+ 上去，等到后面我们再想办法去构造树的时候，我们会再把这些 text 给它拼起来，在检测到一个左尖括号 < 的时候，我们是
+ 不知道这个时候它的字符它到底是一个什么样的 tag 的，所以说这一步暂时我们什么都不做，我们把它进入到 tagOpen 状态，
+ 在 tagOpen 状态，我们就可以去做一些事情了，一旦我们发现它是一个 start tag , 就是没有遇到斜杠，那就是一个
+ 大于号 > 开头，然后后面有一个字母，不管是什么字母，不管是 a 还是 div 的 d 我们都可以确定，它是一个开始标签或者
+ 是一个自封闭标签，那么所以我们就会给 currentToken 赋一个初值，那么给它指一个 type:"starTag" ,有同学就会问
+ 说你 type 为什么不把 selfClosingTag 给它放进去，那么我这里在数据结构上我会选择，不管是自封闭的还是不是自封闭
+ 的，我们都会把它称作 startTag ，而如果是自封闭的，我们用一个额外的变量叫做 isSelfClosing 来标识，所以说这个
+ 地方我们创建 currentToken 的时候，就什么都不用管，直接创一个 type 为 startTag ，Tag 里面它会有 tagName。
+ 然后接下来我们来继续，这个到 tagName 状态的逻辑跟我们前面没有任何的区别，我们只是在这里加了一段逻辑，所以
+ 说我们整个状态的迁移关系都跟我们，前面第三步的代码是一摸一样的，到了在 engTagOpen 的状态，那么我们就会创造一个
+ engTag 标签 token ,然后我们下一步我们就会在 tagName 状态，我们来处理它，
+ 好，接下来就是我们的核心逻辑了，在 TagName 状态下，我们会收到一个标签名组成的字符，如果这个字符它属于字母
+ 的话，我们就会把它追加到当前的 token 的 tagName 上面，然后 beforeAttributeName， 我们仍然是没有去处理，
+ 然后 selfClosingStartTag 我们也是加了一个处理的逻辑，就是这个变量，我们给它置为了 true ,接下来我们还是
+ 实际的跑起来。
+
+
+ 第四步总结
+    在状态机中，除了状态迁移，我们还会要加入业务逻辑，
+    我们在标签结束状态提交标签 token
+
+
+    第四步总结，我觉得主要需要大家掌握的就是在状态机中，咱们除了状态迁移，咱们还需要加入咱们自己的业务逻辑，
+ 在 parser 的代码里面，咱们所谓的业务逻辑就是创建 token ,然后把字符加到 token 上，然后最后 emit token,
+ 咱们在标签的结束状态就会提交标签 token , 注意：这不是在结束标签才提交整个的 token ,我们的开始标签和结束
+ 标签，其实在我们的词法的角度来讲，是两个不同的 token ,中间是一堆文本节点，我们还没有去构建树形的结构，这就
+ 是我们的第 4 个步骤了。
+
  * ***/
 
 
-/***
-第三步总结
-    主要的标签有：开始标签、结束标签 和 自封闭标签
-    在这一步我们暂时忽略属性
- ***/
+
 
 
 
---
+++ Week_07---浏览器工作原理/test/test.js
@@ -0,0 +1,47 @@
+
+var data = {
+    products: [  {child: [{id: 1}, {id: 3}]},    {child: [{id: 4}, {id: 5}]}  ],
+    _index: 1
+};
+function getField(object, key) {
+    let parts = key.split('.');
+    if (parts.length == 1) {
+        let index1 = key.indexOf('[')
+        let index2 = key.indexOf(']')
+        if (index1 > 0 && index2 - index1 > 1) {
+            let field = key.substr(0, index1);
+            let f = object[field];
+            if (f == null) {
+                return null;
+            }
+            if (f instanceof Array) {
+                let indexName = key.substr(index1 + 1, index2 - index1 - 1);
+                let index = parseInt(indexName);
+                if(isNaN(index)) {
+                    index = getField(object, indexName);
+                    if(index == null) {
+                        return null;
+                    }
+                }
+                if (f.length > index) {
+                    return f[index];
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+        return object[key];
+    } else {
+        let right = parts.pop();
+        let left = parts.join('.');
+        let parent = getField(object, left);
+        if(parent == null) {
+            return null;
+        } else {
+            return getField(parent, right);
+        }
+    }
+}
+console.log(getField(data, "products[_index].child[0].id"));

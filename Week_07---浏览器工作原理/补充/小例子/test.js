e6be02ca382afedb78bbd1e7997446cf79917fd7
王帅 <wangshuaishuai@yidiansongli.com>
2020-10-14 16:38:59 +0800

    CSS 计算

--- Week_07---浏览器工作原理/补充/小例子/test.js
+++ Week_07---浏览器工作原理/补充/小例子/test.js
@@ -1,11 +1,25 @@
 // 状态模式
 let data = {
-    products: [  {child: [{id: 1}, {id: 3}]},    {child: [{id: 4}, {id: 5}]}  ],
+    products: [{child: [{id: 1}, {id: 3}]}, {child: [{id: 4}, {id: 5}]}],
     _index: 1
 };
+
 function getField(object, key) {
     let parts = key.split('.');
-    if (parts.length == 1) {
+    return parts.length === 1 ? partsLengthEqualOne() : partsLengthNoEqualOne();
+
+    function partsLengthNoEqualOne() {
+        let right = parts.pop();
+        let left = parts.join('.');
+        let parent = getField(object, left);
+        if (parent == null) {
+            return null;
+        } else {
+            return getField(parent, right);
+        }
+    }
+
+    function partsLengthEqualOne() {
         let index1 = key.indexOf('[')
         let index2 = key.indexOf(']')
         if (index1 > 0 && index2 - index1 > 1) {
@@ -17,9 +31,9 @@ function getField(object, key) {
             if (f instanceof Array) {
                 let indexName = key.substr(index1 + 1, index2 - index1 - 1);
                 let index = parseInt(indexName);
-                if(isNaN(index)) {
+                if (isNaN(index)) {
                     index = getField(object, indexName);
-                    if(index == null) {
+                    if (index == null) {
                         return null;
                     }
                 }
@@ -33,15 +47,11 @@ function getField(object, key) {
             }
         }
         return object[key];
-    } else {
-        let right = parts.pop();
-        let left = parts.join('.');
-        let parent = getField(object, left);
-        if(parent == null) {
-            return null;
-        } else {
-            return getField(parent, right);
-        }
     }
 }
+
+console.log(getField(data, "products"));
 console.log(getField(data, "products[_index].child[0].id"));
+console.log(getField(data, "products[_index].child[1].id"));
+console.log(getField(data, "products[0].child[0].id"));
+console.log(getField(data, "products[0].child[_index].id"));

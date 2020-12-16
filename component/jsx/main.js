2b3b8acc0df2447772acfbe263dd2e2398f6fcf6
王帅 <wangshuaishuai@yidiansongli.com>
2020-12-16 16:59:36 +0800

    组件

--- component/jsx/main.js
+++ component/jsx/main.js
@@ -20,12 +20,16 @@ class Carousel extends Component {
         }
 
         this.root.addEventListener("mousedown", event => {
-            console.log('mousedown');
+            let children = this.root.children;
+            let startX = event.clientX
+            let position = 0;
 
             let move = event => {
-                console.log('mousemove');
-                console.log(event.clientX);
-                console.log(event.clientY);
+                let x = event.clientX - startX;
+                for(let child of children){
+                    child.style.transition = "none";
+                    child.style.transform = `translateX(${ -position * 500 + x}px)`;
+                }
             }
             let up = event=>{
                 console.log('mouseup');
@@ -69,11 +73,18 @@ class Carousel extends Component {
 
 
 let d = [
-    "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
-    "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
-    "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
-    "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+    "../image/1.jpg",
+    "../image/2.jpg",
+    "../image/3.jpg",
+    "../image/4.jpg",
 ];
+
+// let d = [
+//     "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+//     "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+//     "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+//     "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+// ];
 
 let a = <Carousel src={d}/>;
 

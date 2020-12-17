644b4a593a7689d1d528e938f41420846a7321af
王帅 <wangshuaishuai@yidiansongli.com>
2020-12-17 16:12:04 +0800

    数据结构 队列

---
+++ z.js数据结构与算法/队列/1 实现/1 创建队列.js
@@ -0,0 +1,42 @@
+// dequeue  出列
+// enqueue  入列
+
+class Queue{
+    constructor() {
+        this.items = [];
+    }
+    enqueue (element) {
+        this.items.push(element);
+    };
+    dequeue () {
+        return this.items.shift();
+    };
+    front () {
+        return this.items[0];
+    };
+    isEmpty () {
+        return this.items.length == 0;
+    };
+    clear () {
+        this.items = [];
+    };
+    size () {
+        return this.items.length;
+    };
+    print () {
+        console.log(this.items.toString());
+    }
+}
+
+
+let queue = new Queue();
+console.log(queue.isEmpty()); //输出true
+queue.enqueue("John");
+queue.enqueue("Jack");
+queue.print();
+
+console.log(queue.size()); //输出3
+console.log(queue.isEmpty()); //输出false
+queue.dequeue();
+// queue.dequeue();
+queue.print();

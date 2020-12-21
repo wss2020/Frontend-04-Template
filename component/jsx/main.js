e4c797f4c739cef6527216ad0ba221305f02f64b
王帅 <wangshuaishuai@yidiansongli.com>
2020-12-21 18:00:11 +0800

    重学

--- component/jsx/main.js
+++ component/jsx/main.js
@@ -25,17 +25,26 @@ class Carousel extends Component {
             let startX = event.clientX
             let move = event => {
                 let x = event.clientX - startX;
-                for(let child of children){
-                    child.style.transition = "none";
-                    child.style.transform = `translateX(${ -position * 500 + x}px)`;
+
+                let current = position - ((x - x%500 )/ 500);
+                for(let offset of [-1,0,1]){
+                // for(let offset of [-2，-1,0,1，2]){  // 为了避免奇特的bug,算的范围大一点也没有问题。
+                    let pos = current + offset;
+                    pos = (pos + children.length) % children.length;
+
+                    children[pos].style.transition = "none";
+                    children[pos].style.transform = `translateX(${ -pos*500 + offset*500 + x%500}px)`;
                 }
             }
             let up = event=>{
                 let x = event.clientX - startX;
                 position = position - Math.round(x / 500);
-                for(let child of children){
-                    child.style.transition = "";
-                    child.style.transform = `translateX(${ -position * 500}px)`;
+                for(let offset of [0,-Math.sign( Math.round( x/500 ) - x + 250 * Math.sign(x) )]){
+                    let pos = position + offset;
+                    pos = (pos + children.length) % children.length;
+
+                    children[pos].style.transition = "";
+                    children[pos].style.transform = `translateX(${ -pos*500 + offset*500}px)`;
                 }
                 document.removeEventListener("mousemove",move)
                 document.removeEventListener("mouseup",up)

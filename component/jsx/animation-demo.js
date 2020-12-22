54142cd22ce3141cabad09f5c13f3dc0d3a93b9a
王帅 <wangshuaishuai@yidiansongli.com>
2020-12-22 17:21:04 +0800

    13周

---
+++ component/jsx/animation-demo.js
@@ -0,0 +1,12 @@
+import {Animation, Timeline} from "./animation";
+
+let tl = new Timeline();
+
+tl.start();
+
+tl.add(new Animation(
+    document.querySelector("#el").style,
+    'transform',0,500,2000,0,null,v=>`translateX(${v}px)`))
+
+document.querySelector('#pause-btn').addEventListener('click',()=> tl.pause());
+document.querySelector('#resume-btn').addEventListener('click',()=> tl.resume());

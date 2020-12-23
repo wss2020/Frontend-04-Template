34633e106a74bc7379ca1c9c7cfa6c649220ff05
王帅 <wangshuaishuai@yidiansongli.com>
2020-12-23 14:06:25 +0800

    13周

--- component/jsx/animation.js
+++ component/jsx/animation.js
@@ -7,11 +7,15 @@ const PAUSE_TIME = Symbol('pause-time');
 
 export class Timeline {
     constructor() {
+        this.state = "Inited";
         this[ANIMATIONS] = new Set();
         this[START_TIME] = new Map();
     }
 
     start() {
+        if (this.state !== "Inited")
+            return;
+        this.state = "started";
         let startTime = Date.now();
         this[PAUSE_TIME] = 0;
         this[TICK] = () => {
@@ -39,17 +43,24 @@ export class Timeline {
     }
 
     pause() {
+        if(this.state !== "started")
+            return ;
+        this.state = "paused";
         this[PAUSE_START] = Date.now();
         cancelAnimationFrame(this[TICK_HANDLER]);
     }
 
     resume() {
+        if(this.state !== "paused")
+            return ;
+        this.state = "started";
         this[PAUSE_TIME] += Date.now() - this[PAUSE_START];
         this[TICK]();
     }
 
     reset() {
         this.pause();
+        this.state = "Inited";
         let startTime = Date.now(); // 这样写没有用
         this[PAUSE_TIME] = 0;
         this[ANIMATIONS] = new Set();

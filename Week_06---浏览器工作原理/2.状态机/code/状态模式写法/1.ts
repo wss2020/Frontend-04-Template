576de5e6a4864bb6e4699f72019fd0883a6a8043
王帅 <wangshuaishuai@yidiansongli.com>
2021-01-28 16:39:07 +0800

    状态机

--- Week_06---浏览器工作原理/2.状态机/code/状态模式写法/1.ts
+++ Week_06---浏览器工作原理/2.状态机/code/状态模式写法/1.ts
@@ -1,4 +1,6 @@
 /**
+     在一个字符串中，找到字符 "abcdef"
+
      基本状态类声明所有具体状态都应使用的方法实现，并提供对与State相关联的Context对象的反向引用。
  各国可以使用此反向引用来过渡与另一个国家的关系。
  */
@@ -36,36 +38,35 @@ class Context {
 
 
 /** 具体状态实现与上下文状态相关的各种行为。 */
-class ConcreteStateA extends State {
+class FoundA extends State {
     public handle1(): void {
-        console.log('ConcreteStateA 处理 request1。.');
-        console.log('ConcreteStateA 想要更改上下文状态');
+        console.log('FoundA 处理 request1。.');
+        console.log('FoundA 想要更改上下文状态');
         console.log('------------------------------');
-        this.context.transitionTo(new ConcreteStateB());
+        this.context.transitionTo(new FoundB());
     }
     public handle2(): void {
-        console.log('ConcreteStateA 处理 request2.');
+        console.log('FoundA 处理 request2.');
         console.log('------------------------------');
     }
 }
 
 
-class ConcreteStateB extends State {
+class FoundB extends State {
     public handle1(): void {
-        console.log('ConcreteStateB 处理 request1.');
+        console.log('FoundB 处理 request1.');
         console.log('------------------------------');
     }
     public handle2(): void {
-        console.log('ConcreteStateB 处理 request2.');
-        console.log('ConcreteStateB 想要更改上下文状态.');
+        console.log('FoundB 处理 request2.');
+        console.log('FoundB 想要更改上下文状态.');
         console.log('------------------------------');
-        this.context.transitionTo(new ConcreteStateA());
+        this.context.transitionTo(new FoundA());
     }
 }
 
 /**
  * 客户端代码。
  */
-const context = new Context(  new ConcreteStateA()  );
+const context = new Context(  new FoundA()  );
 context.request1();
-//context.request2();
